7fd231a674d60dcb1714212eb1d4dba152aa6bcc
Alessandro Venturini <ales.ventus21@gmail.com>
2025-12-04 11:59:18 +0100

    conversion for Refs, tried List and LazyList

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -7,7 +7,7 @@ import scala.concurrent.*
 import scala.util.Try
 
 case class Ref[A](id: String):
-  def value[F[_]: Monad](using Context[F]): A =
+  def value[F[_]](using Context[F]): A =
     summon[Context[F]].refs(this).get.asInstanceOf[A]
 
 object Ref:
@@ -15,8 +15,8 @@ object Ref:
   // def newRef[A]: Ref[A] = Ref(UUID.randomUUID.toString())
   def newRef[A]: Ref[A] = { count += 1; Ref(count.toString()) }
 
-// given [F[_]: Monad, A](using Context[F]): Conversion[Ref[A], A] with
-//   def apply(x: Ref[A]): A = x.value
+given [F[_], A](using Context[F]): Conversion[Ref[A], A] with
+  def apply(x: Ref[A]): A = x.value
 
 class Context[F[_]: Monad]:
   var refs = MMap[Ref[?], Option[?]]()
@@ -101,6 +101,16 @@ given Monad[Option] with
   extension [A](fa: Option[A])
     def flatMap[B](f: A => Option[B]): Option[B] = fa.flatMap(f)
 
+given Monad[List] with
+  def pure[A](a: A): List[A] = List(a)
+  extension [A](fa: List[A])
+    def flatMap[B](f: A => List[B]): List[B] = fa.flatMap(f)
+
+given Monad[LazyList] with
+  def pure[A](a: A): LazyList[A] = LazyList(a)
+  extension [A](fa: LazyList[A])
+    def flatMap[B](f: A => LazyList[B]): LazyList[B] = fa.flatMap(f)
+
 given (using ec: ExecutionContext): Monad[Future] with
   def pure[A](a: A): Future[A] = Future(a)(using ec)
   extension [A](fa: Future[A])
@@ -159,8 +169,23 @@ object Test extends App:
     async[Option, Int]:
       val a = sync(3)
       val b = await(Option(2))
-      sync(a.value + b.value)
+      sync(a + b)
     .get == 5
+
+  // // THIS ONE FAILS: List(4, 6, 4, 6)
+  // assert:
+  //   (async[List, Int]:
+  //     val a = await(List(1, 2))
+  //     val b = await(List(2, 4))
+  //     sync(a + b)
+  //   ) == List(2, 2, 4, 4)
+
+  assert:
+    async[LazyList, Int]:
+      val a = await(LazyList(1, 2))
+      val b = await(LazyList(2, 4))
+      sync(a + b)
+    .force == List(3, 5, 4, 6)
 
   assert:
     async[Option, Int]:
@@ -171,17 +196,9 @@ object Test extends App:
 
   assert:
     async[Option, Int]:
-      val a = sync(false)
-      loop(a.value):
-        sync(println("loop"))
-      sync(5)
-    .get == 5
-
-  assert:
-    async[Option, Int]:
       val a = sync(3)
-      loop(a.value < 7):
-        assign(a, a.value + 1)
+      loop(a < 7):
+        assign(a, a + 1)
       a
     .get == 7
 
@@ -193,17 +210,23 @@ object Test extends App:
     await(Future(Thread.sleep(300)))
     sync(println("2"))
 
+  println()
+  Thread.sleep(1000)
+
   async[Future, Int]:
     sync(println("0"))
     val a = await(Future({ Thread.sleep(300); 3 }))
     sync(println("1"))
-    // await(Future(Thread.sleep(300)))
+    await(Future(Thread.sleep(300)))
     sync(println("2"))
     a
   .onComplete(res => assert(res == Try(3)))
 
+  println()
+  Thread.sleep(1000)
+
   val f = Future(Thread.sleep(1))
   async[Option, Int]:
     loop(!f.isCompleted):
-      println("loop")
+      sync(println("loop"))
     sync(3)
